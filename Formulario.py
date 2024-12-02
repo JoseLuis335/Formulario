8d399081820d04fd9a9cf2bd4393e1a2cbcd0733
JoseLuis335 <88217428+JoseLuis335@users.noreply.github.com>
2024-12-02 16:38:43 -0600

    Update Formulario.py

--- Formulario.py
+++ Formulario.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 
 # Inicializar Firebase solo si no está ya inicializada
 if not firebase_admin._apps:
-    cred = credentials.Certificate("formulario-211c0-firebase-adminsdk-3tiuf-c2a601f827.json")
+    cred = credentials.Certificate("formulario-211c0-firebase-adminsdk-3tiuf-1b8c819c76.json")
     firebase_admin.initialize_app(cred)
 
 # Conectar a Firestore
