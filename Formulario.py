963a2706de9a10228bdb363f29635bd8fd990d03
JoseLuis335 <88217428+JoseLuis335@users.noreply.github.com>
2024-12-03 09:10:58 -0600

    Update Formulario.py

--- Formulario.py
+++ Formulario.py
@@ -4,7 +4,18 @@ from firebase_admin import credentials, firestore
 
 # Inicializar Firebase solo si no está ya inicializada
 if not firebase_admin._apps:
-    cred = credentials.Certificate("formulario-211c0-firebase-adminsdk-3tiuf-b3f5f668be.json")
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    })
     firebase_admin.initialize_app(cred)
 
 # Conectar a Firestore
